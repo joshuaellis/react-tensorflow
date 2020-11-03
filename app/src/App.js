ceac76a392342a721fb1d6bc09a3fe123ca32cb2
Josh Ellis <joshua.ellis18@gmail.com>
2020-11-03 16:20:01 +0000

    eject CRA

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
 
 function App () {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
