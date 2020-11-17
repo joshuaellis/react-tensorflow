9245ffae7d4ff834245f59eb22349c262fe8e117
Josh Ellis <joshua.ellis18@gmail.com>
2020-11-17 22:36:25 +0000

    chore: fix build

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,4 +1,5 @@
-import rimraf from 'rimraf'
+/* eslint-disable @typescript-eslint/no-var-requires */
+const rimraf = require('rimraf')
 
 rimraf('dist/__test__/**', {}, () => console.log('done global __test__'))
 rimraf('dist/**/__test__/**', {}, () => console.log('done in src __test__'))
